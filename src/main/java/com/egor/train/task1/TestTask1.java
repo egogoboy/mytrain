cee0555a7cba4987c4cdbcac40fdcfafe84c16bc
egogoboy <117520118+egogoboy@users.noreply.github.com>
2023-11-30 21:38:49 +0300

    add util class and Pair

---
+++ src/main/java/com/egor/train/task1/TestTask1.java
@@ -0,0 +1,22 @@
+/*package com.egor.train.task1;
+
+public class TestTask1 {
+    @Test
+    public void shouldReturnSuccessesOnSimpleTest()  {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        Point c = new Point(0, 2);
+        double result = Square.calculatePointSquare(a, b ,c);
+        Assert.assertEquals(2, result, 0);
+    }
+
+    @Test
+    public void shouldReturnErrorOnSimpleTest()  {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        Point c = new Point(0, 2);
+        double result = Square.calculatePointSquare(a, b ,c);
+        Assert.assertNotEquals(3, result, 0);
+    }
+}
+*/
