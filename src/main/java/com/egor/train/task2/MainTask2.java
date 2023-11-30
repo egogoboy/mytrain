6871c1ab5946fd1a3b3820b9a82bdc19809e6dcd
egogoboy <117520118+egogoboy@users.noreply.github.com>
2023-11-30 20:52:13 +0300

    fixed remarks, added a scrap class

--- src/main/java/com/egor/train/task2/MainTask2.java
+++ src/main/java/com/egor/train/task2/MainTask2.java
@@ -1,23 +1,18 @@
 package com.egor.train.task2;
 import java.util.*;
+import static com.egor.train.task2.MyUtil.Util.UpSize;
+
 public class MainTask2 {
     public static void main (String[] args) {
-        System.out.print("Enter the size: ");
-        Scanner in = new Scanner(System.in);
-        double n = in.nextDouble();
-        int type = 1;
-        while (n / 1024 >= 1) {
-            n /= 1024;
-            type++;
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("Enter the size: ");
+            String[] formats = {"B", "KB", "MB", "GB", "TB", "PB"};
+            Pair<Double, Integer> size = new Pair<>();
+            size.first = in.nextDouble();
+            size.second = 0;
+            size = UpSize(size.first, size.second);
+            System.out.printf("%.1f ", size.first);
+            System.out.print(formats[size.second]);
         }
-        System.out.printf("%.1f", n);
-        switch (type) {
-            case 1: System.out.print(" B"); break;
-            case 2: System.out.print(" KB"); break;
-            case 3: System.out.print(" MB"); break;
-            case 4: System.out.print(" GB"); break;
-            case 5: System.out.print(" TB"); break;
-        }
-        in.close();
     }
 }
---
+++ src/main/java/com/egor/train/task2/Pair.java
@@ -0,0 +1,6 @@
+package com.egor.train.task2;
+
+public class Pair<T1, T2> {
+    public T1 first;
+    public T2 second;
+}
